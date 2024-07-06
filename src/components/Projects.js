38502f015c3aef8f55c279584b0b7406f62a3fcc
MasonLiao319 <alienml@yahoo.com>
2024-07-05 23:31:16 -0300

    project/skills

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,14 +1,11 @@
 import { Container, Row, Col } from "react-bootstrap";
-import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
-import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
 export const Projects = () => {
-
   const projects = [
     {
       title: "Project 1",
@@ -34,27 +31,25 @@ export const Projects = () => {
           <Col size={12}>
             <TrackVisibility>
               {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>Explore my portfolio of independently completed projects, demonstrating my expertise in web development, Python programming, and network security.</p>
-                <Row className="project-cards-container">
-                  {
-                    projects.map((project, index) => {
-                      return (
-                        <ProjectCard
-                          key={index}
-                          {...project}
-                        />
-                      )
-                    })
-                  }
-                </Row>
-              </div>}
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  <h2>Projects</h2>
+                  <p>Explore my portfolio of independently completed projects, demonstrating my expertise in web development, Python programming, and network security.</p>
+                  <div className="project-card-container">
+                    {projects.map((project, index) => (
+                      <div className="project-card proj-imgbx" key={index}>
+                        <img src={project.imgUrl} alt={project.title} />
+                        <div className="proj-txtx">
+                          <h4>{project.title}</h4>
+                          <span>{project.description}</span>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2} alt="background" />
     </section>
   )
 }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,16 +1,22 @@
+import React from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import colorSharp from "../assets/img/banner-bg.jpg";
 
+const CustomLeftArrow = ({ onClick }) => {
+  return <div className="custom-left-arrow" onClick={onClick} />;
+};
 
-import colorSharp from "../assets/img/banner-bg.jpg"
+const CustomRightArrow = ({ onClick }) => {
+  return <div className="custom-right-arrow" onClick={onClick} />;
+};
 
 export const Skills = () => {
   const responsive = {
     superLargeDesktop: {
-      // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
       items: 5
     },
@@ -30,45 +36,49 @@ export const Skills = () => {
 
   return (
     <section className="skill" id="skills">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="skill-bx wow zoomIn">
-                        <h2>Skills</h2>
-                        <p>Below are the skills I have acquired and my proficiency levels in each.<br></br>Use the arrows to navigate through all the skills and see more details about my expertise.</p>
-                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-
-                            <div className="item">
-                                <img src={meter1} alt="95" />
-                                <h5>Web Design</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="95" />
-                                <h5>Web Development</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="95" />
-                                <h5>Java</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="95" />
-                                <h5>python</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="90" />
-                                <h5>C++</h5>
-                            </div>                                   
-                            <div className="item">
-                                <img src={meter2} alt="80" />
-                                <h5>C#</h5>
-                            </div>
-                                       
-                        </Carousel>
-                    </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="skill-bx wow zoomIn">
+              <h2>Skills</h2>
+              <p>Below are the skills I have acquired and my proficiency levels in each.<br></br>Use the arrows to navigate through all the skills and see more details about my expertise.</p>
+              <Carousel 
+                responsive={responsive} 
+                infinite={true} 
+                className="owl-carousel owl-theme skill-slider"
+                customLeftArrow={<CustomLeftArrow />}
+                customRightArrow={<CustomRightArrow />}
+              >
+                <div className="item">
+                  <img src={meter1} alt="95" />
+                  <h5>Web Design</h5>
                 </div>
+                <div className="item">
+                  <img src={meter1} alt="95" />
+                  <h5>Web Development</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="95" />
+                  <h5>Java</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="95" />
+                  <h5>Python</h5>
+                </div>
+                <div className="item">
+                  <img src={meter3} alt="90" />
+                  <h5>C++</h5>
+                </div>
+                <div className="item">
+                  <img src={meter2} alt="80" />
+                  <h5>C#</h5>
+                </div>
+              </Carousel>
             </div>
+          </div>
         </div>
-        <img className="background-image-left" src={colorSharp} alt="color" />
+      </div>
+      <img className="background-image-left" src={colorSharp} alt="color" />
     </section>
-  )
+  );
 }
